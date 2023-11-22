42b25df08af1cd586653ff15d14c13cbc287688c
Matt Moretti <werebus@gmail.com>
2023-11-22 17:44:59 -0500

    Install new Ruby 3.x

--- Casks/umts-rbenv.rb
+++ Casks/umts-rbenv.rb
@@ -12,16 +12,25 @@ cask 'umts-rbenv' do
   depends_on formula: 'rbenv'
 
   postflight do
-    next if Utils.rbenv_is_function?
-
-    File.open(Utils.rcfile, 'w+') do |f|
-      f.puts %Q|eval "$(rbenv init - #{Utils.shell})"|
+    unless Utils.rbenv_is_function?
+      File.open(Utils.rcfile, 'w+') do |f|
+        f.puts %Q|eval "$(rbenv init - #{Utils.shell})"|
+      end
     end
+
+    version = Utils.ruby_version
+    global_file = Utils.home.join '.rbenv', 'version'
+    unless global_file.exist?
+      global_file.write "#{version}\n"
+    end
+
+    system 'rbenv', 'install', '--skip-existing', version
   end
 
   module Utils
     require 'etc'
     require 'pathname'
+    require 'rubygems'
 
     module_function
 
@@ -46,6 +55,11 @@ cask 'umts-rbenv' do
       when 'bash' then home.join('.bash_profile')
       else '/dev/null'
       end
+    end
+
+    def ruby_version
+      versions = `sh -l -c 'rbenv install --list'`.split.select { |v| /^[0-9.]+$/.match? v }
+      versions.max_by { |v| Gem::Version.new(v) }
     end
 
     def shell
