e56044eec989147a5002ae7b324cc21fb72d0252
Matt Moretti <werebus@gmail.com>
2023-11-26 16:03:48 -0500

    Some Shell utils

--- lib/shell_utils.rb
+++ lib/shell_utils.rb
@@ -1,2 +1,20 @@
+require 'etc'
+require 'pathname'
+
 module ShellUtils
+  def home
+    Pathname(ENV['HOME'])
+  end
+
+  def rcfile
+    case shell
+    when 'zsh' then home.join('.zshrc')
+    when 'bash' then home.join('.bash_profile')
+    else '/dev/null'
+    end
+  end
+
+  def shell
+    Etc.getpwuid.shell.split('/').last
+  end
 end
